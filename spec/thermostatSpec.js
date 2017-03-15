fd2b07300fa34aeda8c4343e49661ef5f2b841bd
Eva Balog <evebalog@netscape.net>
2017-03-15 15:25:51 +0000

    First commit

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,10 @@
+// describe('Playing fizzbuzz', function() {
+//   var blabla;
+//   blabla = new fizzBuzz();
+//
+//   it("returns fizz if number is divisable by 3", function() {
+//     console.log(blabla)
+//     expect(blabla.doTheFizzbuzz(3)).toBe("Fizz");
+//   });
+//
+// });
