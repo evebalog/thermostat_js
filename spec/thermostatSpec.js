25b7fb746936225fc3e78b59c29a961215451f34
Eva Balog <evebalog@netscape.net>
2017-03-15 17:58:27 +0000

    Added max temps, reset and power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Thermostat', function() {
   var thermostat;
 
@@ -20,11 +22,37 @@ describe('Thermostat', function() {
   });
 
   it("throws an error if you try to reduce temperature while current temperature is 10", function() {
-    // var times = 10;
-    // for(var i=0; i < times; i++){
-    //   thermostat.down();
-    // }
+
     spyOn(thermostat, "getTemp").and.returnValue(10)
     expect(function(){thermostat.down();}).toThrowError("Temperature is at minimum");
   });
+
+  it("checks power saving mode is on by default", function() {
+    expect(thermostat.mode()).toEqual(true)
+  })
+
+  it("can turn on power saving mode", function() {
+    thermostat.changeMode();
+    expect(thermostat.mode()).toEqual(false)
+  })
+
+  it("doesn't go above 25 degrees if power save mode is on", function() {
+    spyOn(thermostat, "getTemp").and.returnValue(25)
+    expect(function(){thermostat.up();}).toThrowError("Temperature is at maximum");
+  })
+
+  it("doesn't go above 32 degrees if power save mode is off", function() {
+    spyOn(thermostat, "getTemp").and.returnValue(32)
+    thermostat.changeMode()
+    expect(function(){thermostat.up();}).toThrowError("Temperature is at maximum");
+  })
+
+  it("can reset temperature to 20 degrees by reset function", function() {
+    var times = 5;
+    for(var i=0; i < times; i++){
+      thermostat.up();
+    }
+    thermostat.reset()
+    expect(thermostat.getTemp()).toEqual(20)
+  })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,18 +1,47 @@
+'use strict';
+
 function Thermostat() {
   this.currentTemp = 20;
+  this.currentMode = true;
+  this.MINIMUMTEMP = 10
+  this.PSMAXIMUMTEMP = 25
+  this.MAXIMUMTEMP = 32
 }
+
+Thermostat.prototype.mode = function () {
+  return this.currentMode;
+};
+
+Thermostat.prototype.changeMode = function () {
+   this.currentMode = !this.currentMode;
+   if(this.getTemp() > 25 ) {
+     this.currentTemp = 25
+   }
+};
 
 Thermostat.prototype.getTemp = function () {
   return this.currentTemp;
 };
 
 Thermostat.prototype.up = function () {
+  if(this.mode() === false) {
+    var max = this.MAXIMUMTEMP;
+  } else {
+    var max = this.PSMAXIMUMTEMP;
+  }
+  if(this.getTemp() === max) {
+  throw new Error("Temperature is at maximum");
+}
   this.currentTemp += 1;
 };
 
 Thermostat.prototype.down = function () {
-  if(this.getTemp() === 10) {
+  if(this.getTemp() === this.MINIMUMTEMP) {
   throw new Error("Temperature is at minimum");
 }
   this.currentTemp -= 1;
+};
+
+Thermostat.prototype.reset = function () {
+  this.currentTemp = 20
 };
